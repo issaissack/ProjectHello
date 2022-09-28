caf7e06397057251558c4e1d25928e78ee02594f
issa <92391392+essaissa@users.noreply.github.com>
2022-09-27 22:27:34 -0300

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello There!");
+        System.out.println("This is way easier than I thought");
+    }
+
+}
